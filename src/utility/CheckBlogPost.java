4651b74e5dce72c9f303c681a1240d7b7de94c0e
SIVANESAN-DHANIKACHALAM <61906309+Sivanesan711@users.noreply.github.com>
2020-03-29 14:02:40 +0530

    Add files via upload

--- src/utility/CheckBlogPost.java
+++ src/utility/CheckBlogPost.java
@@ -1,1 +1,60 @@
 package utility;
+
+import model.Blog;
+
+public class CheckBlogPost extends OffensiveWordsChecker implements OffensiveWordsInterface{
+
+	@Override
+	public boolean checkBlogTitle(Blog blog) {
+		if(!(offensivewords(blog.getBlogTitle())))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+
+	public boolean checkBlogDescription(Blog blog) {
+		if(!(offensivewords(blog.getBlogDescription())))
+		{
+			return true;
+		}
+		else
+		{
+		    return false;
+		}
+	}
+
+	@Override
+	public boolean checkBlog(Blog blog) {
+		if(checkBlogTitle(blog) && checkBlogDescription(blog))
+		{
+			return true;
+		}
+		else
+		{
+		return false;
+		}
+	}
+	
+	public boolean offensivewords(String title)
+	{
+		String titles[] = title.split("");
+		String[] array1 = new String[]{"shit","anal","asshole","worst","bad","arian"};
+		
+		for(int index=0; index<titles.length;index++)
+		{
+			for(int index1=0;index1<array1.length;index1++)
+			{
+				if(titles[index].contentEquals(array1[index1]))
+				{
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+}
--- src/utility/OffensiveWordsChecker.java
+++ src/utility/OffensiveWordsChecker.java
@@ -1,2 +1,19 @@
 package utility;
 
+import model.Blog;
+
+abstract class OffensiveWordsChecker{
+	
+	public abstract boolean checkBlog(Blog blog);
+
+	public boolean checkBlogDescription(Blog blog) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean checkBlogTitle(Blog blog) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
+
